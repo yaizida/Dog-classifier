7120935335a1b9cef54c798baea5ba71e9a97932
izida <myizida@gmail.com>
2024-07-22 22:00:32 +0300

    add alembic

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_title: str = 'Классификация породы собоак'
+    app_title: str = 'Классификация пород собоак'
     description: str = ('Проект для классификации пород собак через обучении' +
                         'модели на scikit-learn')
 
